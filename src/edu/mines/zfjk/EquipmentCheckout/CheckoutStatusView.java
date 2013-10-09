fbee877f1a966a9c0e0d5a26169d8385b6c227c9
John Kelly <johkelly@mines.edu>
2013-10-09 15:57:14 -0600

    Rough detail view layout and custom CheckoutStatusView.

---
+++ src/edu/mines/zfjk/EquipmentCheckout/CheckoutStatusView.java
@@ -0,0 +1,28 @@
+package edu.mines.zfjk.EquipmentCheckout;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: John Kelly
+ * Date: 10/9/13
+ * Time: 3:44 PM
+ */
+public class CheckoutStatusView extends View {
+
+    public CheckoutStatusView(Context context) {
+        super(context);
+    }
+
+    public CheckoutStatusView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CheckoutStatusView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+
+}
