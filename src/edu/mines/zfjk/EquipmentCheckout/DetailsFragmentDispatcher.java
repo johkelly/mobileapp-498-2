e54b31e6401ca8b4fb071ef6a51d57a5c0b410fd
John Kelly <johkelly@mines.edu>
2013-10-17 00:30:50 -0600

    Add missing files

---
+++ src/edu/mines/zfjk/EquipmentCheckout/DetailsFragmentDispatcher.java
@@ -0,0 +1,12 @@
+package edu.mines.zfjk.EquipmentCheckout;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jack
+ * Date: 10/16/13
+ * Time: 11:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface DetailsFragmentDispatcher {
+    void displayDetailsFor(int pos);
+}
